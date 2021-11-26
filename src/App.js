68d9d20d58d1257393d0c9a4797e96db41e2feb2
luisresende13 <luisresende13@gmail.com>
2021-11-26 15:09:53 -0300

    Components Part 2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
+import React, { Component } from 'react';
 import './App.css';
-
-import { Navbar, NavbarBrand } from 'reactstrap';
 
 import Menu from './components/MenuComponent';
 
-import React, { Component } from 'react';
+import { DISHES } from './shared/dishes';
+
+import { Navbar, NavbarBrand } from 'reactstrap';
+
+
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes:DISHES
+    };
+  }
+
   render() {
     return (
       <div className="App">
@@ -17,7 +28,7 @@ class App extends Component {
           </div>
         </Navbar>
 
-        <Menu />
+        <Menu dishes={this.state.dishes} />
 
       </div>
     );
